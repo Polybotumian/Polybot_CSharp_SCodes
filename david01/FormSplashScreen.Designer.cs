6cb106f15cc3260b6f0edb5ff82eab63bad5d0c6
Yiğit Leblebicier <70536978+Polybotumian@users.noreply.github.com>
2021-11-20 01:10:44 +0300

    Add files via upload

---
+++ david01/FormSplashScreen.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Voice_Bot
+{
+    partial class FormSplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorkerSplashScreen = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorkerSplashScreen
+            // 
+            this.backgroundWorkerSplashScreen.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerSplashScreen_RunWorkerCompleted);
+            // 
+            // FormSplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(10, 10);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormSplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormSplashScreen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FormSplashScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorkerSplashScreen;
+    }
+}
